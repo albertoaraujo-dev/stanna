71c879e82c783df280cf071b50542d54c79ca6c5
Alberto Araujo <albertoaraujo.dev@gmail.com>
2024-07-12 22:53:00 -0300

    build beta version

---
+++ build/js/script.js
@@ -0,0 +1,32 @@
+const links = document.querySelectorAll('.header-menu a')
+
+function activeLink(link) {
+    const url = location.href
+    const href = link.href
+    if (url.includes(href)) {
+        link.classList.add('ativo')
+    }
+}
+
+links.forEach(activeLink)
+
+//Animation
+if (window.SimpleAnime) {
+    new SimpleAnime()
+}
+
+//cookies
+if (document.URL.includes('index')) {
+    let cookiesContent = document.getElementById('cookies')
+
+    function acceptCookies() {
+        localStorage.lgpd = 'yes'
+        cookiesContent.classList.remove('show')
+    }
+
+    if (localStorage.lgpd == 'yes') {
+        cookiesContent.classList.remove('show')
+    } else {
+        cookiesContent.classList.add('show')
+    }
+}
